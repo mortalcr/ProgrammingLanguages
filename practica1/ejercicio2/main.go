f9f3f781826791ca5fa999fc3b12038c4678892e
mortalcr <daporrasn@gmail.com>
2023-08-17 17:19:19 -0600

    🥶

--- practica1/ejercicio2/main.go
+++ practica1/ejercicio2/main.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-var num int
+func main() {
+  maxAsterisks := 5
 
-func init(){
-  
+	height := (maxAsterisks + 1) / 2
+
+	for i := 1; i <= height; i++ {
+		for j := 1; j <= height-i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 1; k <= 2*i-1; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+	for i := height - 1; i >= 1; i-- {
+		for j := 1; j <= height-i; j++ {
+			fmt.Print(" ")
+		}
+		for k := 1; k <= 2*i-1; k++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
 }
 
-func main(){
-  fmt.Println(num)
-}
